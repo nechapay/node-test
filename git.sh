041dbfe787b3c3e342a1d95e6f4b17f117bf5a25
pavel <nechapay@list.ru>
2021-10-13 10:49:13 +0300

    first commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# node-test" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nechapay/node-test.git
+git push -u origin main
